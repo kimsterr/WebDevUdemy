197288671e34359e781df42e053d5236cca9b78b
Felix Kim <felix.l.kim@gmail.com>
2022-07-17 12:21:35 -0400

    Finished the lang exercise

--- section32/LanguageGuesser/index.js
+++ section32/LanguageGuesser/index.js
@@ -1,1 +1,20 @@
-console.log("Hello world!")
+import {franc} from 'franc'
+
+const minLength = 10;
+
+const languageNames = new Intl.DisplayNames(['en'], {
+    type: 'language'
+  });
+
+// Grab the arguments
+let foreignPhrase = process.argv[2];
+if (!foreignPhrase) {
+    console.log("Please type in an actual foreign phrase as:  $ node index.js <foreignPhrase>");
+}
+else if (foreignPhrase.length < minLength) {
+    console.log(`Please type a foreign phrase that is at least ${minLength} chars long.`);
+}
+else {
+    let countryCode = franc(foreignPhrase, {minLength: minLength});
+    console.log(languageNames.of(countryCode))
+}
