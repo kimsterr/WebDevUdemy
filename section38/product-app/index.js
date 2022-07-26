75baf433dd69d44106ff5dc2c36ab1a452cf2e7b
Felix Kim <felix.l.kim@gmail.com>
2022-07-26 15:39:59 -0400

    Set up DB connection, set views properties, basic GET request

--- section38/product-app/index.js
+++ section38/product-app/index.js
@@ -3,8 +3,25 @@ const res = require('express/lib/response');
 const app = express();
 const port = 8080;
 const path = require('path');
-const { v4: uuidv4 } = require('uuid');
-const methodOverride = require('method-override');
+const methodOverride = require('method-override'); // For UPDATE capabilities!
+
+const mongoose = require('mongoose')
+const { Schema } = mongoose;
+mongoose.connect('mongodb://localhost:27017/movies')
+    .then(() => {
+        console.log("DB CONNECTION OPEN!");
+    })
+    .catch((err) => {
+        console.log("OH NO THERE IS AN ERROR! WITH THE DB CONNECTION");
+        console.log(err);
+    })
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/dog', (req, res) => {
+    res.send('WOOF!');
+})
 
 app.listen(port, () => {
     console.log(`Example app listening at https://localhost:${port}`);
