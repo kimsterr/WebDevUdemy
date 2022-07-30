08cfa86f28fd2ccf3020017dd54233c9c9e559d5
Felix Kim <felix.l.kim@gmail.com>
2022-07-30 11:05:32 -0700

    Set up the basic Products index

--- section38/product-app/index.js
+++ section38/product-app/index.js
@@ -20,8 +20,9 @@ mongoose.connect('mongodb://localhost:27017/farmStand')
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/dog', (req, res) => {
-    res.send('WOOF!');
+app.get('/products', async (req, res) => {
+    const products = await Product.find({}); // An array of our products
+    res.render('products/index', { products });
 })
 
 app.listen(port, () => {
