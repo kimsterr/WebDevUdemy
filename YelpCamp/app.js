f70d7e39751bbf7eae2343776194e7534a588f94
Felix Kim <felix.l.kim@gmail.com>
2022-08-03 15:53:27 -0700

    Set up YelpCamp connection to DB

---
+++ YelpCamp/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const res = require('express/lib/response');
+const app = express();
+const port = 8080;
+const path = require('path');
+const methodOverride = require('method-override'); // For UPDATE capabilities!
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp')
+    .then(() => {
+        console.log("DB CONNECTION OPEN!");
+    })
+    .catch((err) => {
+        console.log("OH NO THERE IS AN ERROR! WITH THE DB CONNECTION");
+        console.log(err);
+    })
+
+const Campground = require('./models/campground')
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.send("HELLO FROM YELPCAMP!");
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at https://localhost:${port}`);
+})
