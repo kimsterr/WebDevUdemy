030b372300caec1bee84a9a4245a9f24a3db5e5e
Felix Kim <felix.l.kim@gmail.com>
2022-08-02 17:49:12 -0700

    Finished product deletion

--- section38/product-app/index.js
+++ section38/product-app/index.js
@@ -69,6 +69,14 @@ app.put('/products/:id', async (req, res) => {
     res.redirect(`/products/${id}`);
 })
 
+app.delete('/products/:id', async (req, res) => {
+    const id = req.params.id;
+
+    await Product.findByIdAndDelete(id);
+
+    res.redirect('/products');
+})
+
 app.listen(port, () => {
     console.log(`Example app listening at https://localhost:${port}`);
 })
