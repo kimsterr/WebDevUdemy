28e6685293662f756503631bfd46d7a9f568a5b1
Felix Kim <felix.l.kim@gmail.com>
2022-08-16 17:13:59 -0700

    Set up User model along with Passport

---
+++ YelpCamp/models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const passportLocalMongoose = require('passport-local-mongoose');
+const { Schema } = mongoose;
+
+mongoose = require('mongoose')
+
+// Define the schema
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }
+});
+
+// This will add on a field for username and add on a field for password
+userSchema.plugin(passportLocalMongoose)
+
+module.exports = mongoose.model('User', userSchema);
