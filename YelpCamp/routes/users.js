a8023625853e30fe29fdb83a9e526369625f7c1b
Felix Kim <felix.l.kim@gmail.com>
2022-08-18 18:11:50 -0700

    Set up the YelpCamp login routes

--- YelpCamp/routes/users.js
+++ YelpCamp/routes/users.js
@@ -4,6 +4,7 @@ const User = require('../models/user')
 const wrapAsync = require('../utils/catchAsync')
 const validate = require('../utils/validateSchema')
 const { userSchema } = require('../joiSchemas')
+const passport = require('passport')
 
 router.get('/register', (req, res) => {
     res.render('users/register');
@@ -17,5 +18,18 @@ router.post('/register', validate(userSchema), wrapAsync(async (req, res, next) 
     req.flash('success', 'Successfully registered!');
     res.redirect(`/campgrounds`);
 }))
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+})
+
+// 'local' is the strategy, could use other strategies
+router.post('/login',
+    passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }),
+    (req, res) => {
+        req.flash('success', 'Successfully logged in!');
+        res.redirect('/campgrounds');
+    }
+)
 
 module.exports = router;
